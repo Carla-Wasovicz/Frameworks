269a7b674c57cb02e302dae762b72089332e6903
Carla-Wasovicz <carla.almeida2@estudante.ifms.edu.br>
2021-04-28 20:59:25 -0500

    Relacionamento

--- src/main/java/com/ifms/entities/Escola.java
+++ src/main/java/com/ifms/entities/Escola.java
@@ -1,11 +1,13 @@
 package com.ifms.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,6 +18,9 @@ public class Escola implements Serializable{
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
+	@OneToMany(mappedBy = "escola")
+	private List<Equipe> equipes;
+	
 	private String nome;
 	private String endereco;
 	private String site;
